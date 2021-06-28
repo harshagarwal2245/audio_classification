210770de973836005b9c56f7fd8756b463eb5467
harsh agarwal <agarwalharsh244@gmail.com>
2021-06-28 15:23:49 +0530

    Done with modelling

---
+++ notebooks/Eda.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 28 12:36:32 2021
+
+@author: lab-a-27
+"""
+
+import matplotlib.pyplot as plt
+
+import IPython.display as ipd
+import librosa
+import librosa.display
